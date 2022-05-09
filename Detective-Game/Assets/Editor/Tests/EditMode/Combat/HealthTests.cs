f7ff1951e3f5f1883e6e10052c1662ca9743382a
AceDroo <deroom01@gmail.com>
2022-05-09 17:04:34 +1000

    refactor: Update HealthTests

--- Detective-Game/Assets/Editor/Tests/EditMode/Combat/HealthTests.cs
+++ Detective-Game/Assets/Editor/Tests/EditMode/Combat/HealthTests.cs
@@ -7,122 +7,164 @@ namespace MitchDroo.DetectiveGame.Tests.Combat
     public class HealthTests
     {
         [Test]
-        public void SetStartingAndMaxHealth_GivenJustMaxHealth()
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(30)]
+        public void SetStartingAndMaxHealth_GivenJustMaxHealth(int maxHealth)
         {
-            Health health = new Health(10);
-            Assert.AreEqual(10, health.CurrentHealth);
-            Assert.AreEqual(10, health.MaxHealth);
+            Health health = new Health(maxHealth);
+            Assert.AreEqual(maxHealth, health.CurrentHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
+            Assert.IsTrue(health.CurrentHealth <= health.MaxHealth);
+            Assert.IsTrue(health.CurrentHealth != 0);
         }
 
         [Test]
-        public void SetStartingAndMaxHealth_GivenStartingAndMaxHealth()
+        [TestCase(1, 10)]
+        [TestCase(5, 10)]
+        [TestCase(10, 20)]
+        [TestCase(30, 30)]
+        public void SetStartingAndMaxHealth_GivenStartingAndMaxHealth(int currentHealth, int maxHealth)
         {
-            Health health = new Health(10, 10);
-            Assert.AreEqual(10, health.CurrentHealth);
-            Assert.AreEqual(10, health.MaxHealth);
+            Health health = new Health(currentHealth, maxHealth);
+            Assert.AreEqual(currentHealth, health.CurrentHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
+            Assert.IsTrue(health.CurrentHealth <= health.MaxHealth);
+            Assert.IsTrue(health.CurrentHealth != 0);
         }
 
         [Test]
-        public void TakeDamage_AboveZeroHealth_ReturnsDecrementedHealth()
+        [TestCase(5, 1)]
+        [TestCase(10, 5)]
+        [TestCase(20, 10)]
+        public void TakeDamage_AboveZeroHealth_ReturnsDecrementedHealth(int maxHealth, int damage)
         {
-            Health health = new Health(5);
+            Health health = new Health(maxHealth);
 
-            health.TakeDamage(1);
+            health.TakeDamage(damage);
             
-            Assert.AreEqual(5 - 1, health.CurrentHealth);
-            Assert.AreEqual(5, health.MaxHealth);
+            Assert.AreEqual(maxHealth - damage, health.CurrentHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
         }
 
         [Test]
-        public void TakeDamage_BelowZeroHealth_ReturnsZero()
+        [TestCase(1, 1)]
+        [TestCase(5, 10)]
+        [TestCase(10, 20)]
+        public void TakeDamage_BelowZeroHealth_ReturnsZero(int maxHealth, int damage)
         {
-            Health health = new Health(1);
-            health.TakeDamage(10);
+            Health health = new Health(maxHealth);
+            health.TakeDamage(damage);
             Assert.AreEqual(0, health.CurrentHealth);
-            Assert.AreEqual(1, health.MaxHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
         }
 
         [Test]
-        public void TakeDamage_OnHealthChangedIsInvoked()
+        [TestCase(1, 1)]
+        [TestCase(5, 10)]
+        [TestCase(10, 20)]
+        public void TakeDamage_OnHealthChangedIsInvoked(int maxHealth, int damage)
         {
-            Health health = new Health(10);
+            Health health = new Health(maxHealth);
             bool isInvoked = false;
 
             health.OnHealthChanged += (v) => isInvoked = true;
 
-            health.TakeDamage(1);
+            health.TakeDamage(damage);
 
             Assert.IsTrue(isInvoked);
         }
 
         [Test]
-        public void TakeDamage_BelowZeroHealth_OnHealthDepletedIsInvoked()
+        [TestCase(1, 1)]
+        [TestCase(5, 10)]
+        [TestCase(10, 20)]
+        public void TakeDamage_BelowZeroHealth_OnHealthDepletedIsInvoked(int maxHealth, int damage)
         {
-            Health health = new Health(1);
+            Health health = new Health(maxHealth);
             bool isInvoked = false;
             health.OnHealthDepleted += () => isInvoked = true;
 
-            health.TakeDamage(1);
+            health.TakeDamage(damage);
             
             Assert.IsTrue(isInvoked);
         }
 
         [Test]
-        public void TakeDamage_AboveZeroHealth_OnHealthDepletedIsNotInvoked()
+        [TestCase(5, 1)]
+        [TestCase(10, 5)]
+        [TestCase(20, 10)]
+        public void TakeDamage_AboveZeroHealth_OnHealthDepletedIsNotInvoked(int maxHealth, int damage)
         {
-            Health health = new Health(2);
+            Health health = new Health(maxHealth);
             bool isInvoked = false;
             health.OnHealthDepleted += () => isInvoked = true;
 
-            health.TakeDamage(1);
+            health.TakeDamage(damage);
 
             Assert.IsFalse(isInvoked);
         }
 
         [Test]
-        public void ReceiveHealth_BelowMaxHealth_ReturnsIncrementedHealth()
+        [TestCase(1, 10, 1)]
+        [TestCase(5, 20, 10)]
+        [TestCase(10, 50, 20)]
+        public void ReceiveHealth_BelowMaxHealth_ReturnsIncrementedHealth(int currentHealth, int maxHealth, int amount)
         {
-            Health health = new Health(1, 10);
-            health.ReceiveHealth(1);
-            Assert.AreEqual(1 + 1, health.CurrentHealth);
-            Assert.AreEqual(10, health.MaxHealth);
+            Health health = new Health(currentHealth, maxHealth);
+            health.ReceiveHealth(amount);
+            Assert.AreEqual(currentHealth + amount, health.CurrentHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
         }
 
         [Test]
-        public void ReceiveHealth_AboveMaxHealth_ReturnsMaxHealth()
+        [TestCase(1, 10, 20)]
+        [TestCase(5, 20, 50)]
+        [TestCase(10, 50, 100)]
+        public void ReceiveHealth_AboveMaxHealth_ReturnsMaxHealth(int currentHealth, int maxHealth, int amount)
         {
-            Health health = new Health(1, 10);
-            health.ReceiveHealth(20);
-            Assert.AreEqual(10, health.CurrentHealth);
-            Assert.AreEqual(10, health.MaxHealth);
+            Health health = new Health(currentHealth, maxHealth);
+            health.ReceiveHealth(amount);
+            Assert.AreEqual(maxHealth, health.CurrentHealth);
+            Assert.AreEqual(maxHealth, health.MaxHealth);
         }
 
         [Test]
-        public void ReceiveHealth_OnHealthChangedIsInvoked()
+        [TestCase(1, 10, 1)]
+        [TestCase(5, 20, 10)]
+        [TestCase(10, 50, 20)]
+        public void ReceiveHealth_OnHealthChangedIsInvoked(int currentHealth, int maxHealth, int amount)
         {
-            Health health = new Health(1, 10);
+            Health health = new Health(currentHealth, maxHealth);
             bool isInvoked = false;
 
             health.OnHealthChanged += (v) => isInvoked = true;
 
-            health.ReceiveHealth(1);
+            health.ReceiveHealth(amount);
 
             Assert.IsTrue(isInvoked);
         }
 
         [Test]
-        public void IsDead_ZeroHealth_ReturnsTrue()
+        [TestCase(1, 1)]
+        [TestCase(5, 10)]
+        [TestCase(10, 20)]
+        public void IsDead_ZeroHealth_ReturnsTrue(int maxHealth, int damage)
         {
-            Health health = new Health(1);
-            health.TakeDamage(1);
+            Health health = new Health(maxHealth);
+            health.TakeDamage(damage);
             Assert.IsTrue(health.IsDead);
         }
 
         [Test]
-        public void IsDead_MoreThanZeroHealth_ReturnsFalse()
+        [TestCase(5, 1)]
+        [TestCase(10, 5)]
+        [TestCase(20, 10)]
+        public void IsDead_MoreThanZeroHealth_ReturnsFalse(int maxHealth, int damage)
         {
-            Health health = new Health(10);
-            health.TakeDamage(1);
+            Health health = new Health(maxHealth);
+            health.TakeDamage(damage);
             Assert.IsFalse(health.IsDead);
         }
     }
